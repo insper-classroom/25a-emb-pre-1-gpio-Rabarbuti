292faad9faede2b63de622cf3a250c0ff83f4bd3
raphaelbb1 <rabarbuti@gmail.com>
2025-02-11 19:35:02 -0300

    ex2_delay

--- exe2/main.c
+++ exe2/main.c
@@ -20,11 +20,13 @@ int main() {
     if (!gpio_get(BTN_PIN)) {
       printf("Botao 1\n");
       while (!gpio_get(BTN_PIN)) {
+        sleep_ms(10);
       }
     }
     if (!gpio_get(BTN_PIN_2)) {
       printf("Botao 2\n");
       while (!gpio_get(BTN_PIN_2)) {
+        sleep_ms(10);
       }
     }
   }
